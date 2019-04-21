d9826ee14bb964f74172e8c64b74068fbfedf792
hoaidien <dienhh@eplatform.vn>
2019-04-21 23:19:09 +0700

    Add page cart + checkout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  res.render('home');
 });
-
+router.get('/category', function(req, res, next) {
+  res.render('category');
+});
+router.get('/cart', function(req, res, next) {
+  res.render('cart');
+});
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout');
+});
 module.exports = router;
