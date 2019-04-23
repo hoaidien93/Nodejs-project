7abf509c75bd0a39a78f0a064d4e0ea3cbb1ab28
Dien Hoai <hoaidien93@gmail.com>
2019-04-23 09:30:38 +0700

    add link to all page

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
+router.get('/home', function(req, res, next) {
+  res.render('home',{isLogin: true});
 });
 router.get('/category', function(req, res, next) {
-  res.render('category');
+  res.render('category',{isLogin: true});
 });
 router.get('/cart', function(req, res, next) {
-  res.render('cart');
+  res.render('cart',{isLogin: true});
 });
 router.get('/checkout', function(req, res, next) {
-  res.render('checkout');
+  res.render('checkout',{isLogin: true});
 });
 router.get('/product', function(req, res, next) {
-  res.render('product');
+  res.render('product',{isLogin: true});
 });
 router.get('/search', function(req, res, next) {
-  res.render('search');
+  res.render('search',{isLogin: true});
+});
+router.get('/login',(req,res)=>{
+  res.render('login',{isLogin: false});
+});
+router.get('/',(req,res)=>{
+  res.render('login',{isLogin: false});
+});
+router.get('/sign-up',(req,res)=>{
+  res.render('signUp',{isLogin: false});
+});
+router.get('/forgotPass',(req,res)=>{
+  res.render('forgotPass',{isLogin: false});
 });
 module.exports = router;
